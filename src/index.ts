fd45a5c5ecaa3bec4882e5050e0a80ef131def8e
syedkashiftirmizi <79271879+syedkashiftirmizi@users.noreply.github.com>
2023-10-07 09:25:12 +0500

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+let userInput = await inquirer.prompt([
+	{
+		type: "list",
+		name: "Option",
+		choices: ["addition", "sub", "division", "Multiply"],
+        message:"Which Function Do You Want:"
+	},
+    {
+        type:"number",
+        name:"num1",
+        message:"Enter Your First Number: "
+    },
+    {
+        type:"number",
+        name:"num2",
+        message:"Enter Your Second Number: "
+    },
+]);
+
+if (userInput.Option === "addition") {
+    console.log(`${userInput.num1} + ${userInput.num2} = ${userInput.num1 + userInput.num2}`)
+} else if (userInput.Option === "sub") {
+        console.log(`${userInput.num1} - ${userInput.num2} = ${userInput.num1 - userInput.num2}`);
+} else if (userInput.Option === "Multiply") {
+        console.log(`${userInput.num1} x ${userInput.num2} = ${userInput.num1 * userInput.num2}`);
+} else if (userInput.Option === "division") {
+        console.log(`${userInput.num1} ÷ ${userInput.num2} = ${userInput.num1 / userInput.num2}`);
+}
